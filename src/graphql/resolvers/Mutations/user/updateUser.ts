a2f8f46ec2ee367207a210b12ca5098fa2fe939c
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2022-10-24 01:48:43 +0200

    Refactor graphql interface
    With addition of new items like shopping lists and items, the current
    structure will get more and more messy.
    Refactor now to make future extensions easier.

---
+++ src/graphql/resolvers/Mutations/user/updateUser.ts
@@ -0,0 +1,29 @@
+import { User } from "../../../../db/model/User";
+import { DidYouGetLoginData } from "../../../../utils/auth/model";
+import { hashPassword } from "../../../../utils/auth/hashPassword";
+
+type UpdateValues = {
+    username: string;
+    password?: string;
+};
+
+export const updateUser = async (
+    _parent: object,
+    args: { input: { username: string; password: string | undefined } },
+    context: { auth: DidYouGetLoginData }
+) => {
+    const { username, password } = args.input;
+
+    const data = <UpdateValues>{ username: username };
+    if (password) {
+        const hashedPassword = await hashPassword(password);
+        data.password = hashedPassword;
+    }
+
+    await User.update(data, { where: { id: context.auth.userid } });
+    const user = await User.findByPk(context.auth.userid);
+
+    return {
+        success: Boolean(user)
+    };
+};
