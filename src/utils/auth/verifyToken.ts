22ac00961205d3f5c21b1988692419e49e254b65
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2022-10-24 00:25:21 +0200

    Rename class CustomJwtPayload to DidYouGetLoginData
    ... to better reflect what ist the content of the class

--- src/utils/auth/verifyToken.ts
+++ src/utils/auth/verifyToken.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { envHandler } from "../envHandler";
-import { CustomJwtPayload } from "./model";
+import { DidYouGetLoginData } from "./model";
 
 export const verifyToken = (token: string) => {
-    return <CustomJwtPayload>jwt.verify(token, envHandler.get("JWT_SECRET"));
+    return <DidYouGetLoginData>jwt.verify(token, envHandler.get("JWT_SECRET"));
 };
