fc4ed50348c5042e3c13b40dbe17b8fd51aeddda
FelixWohlfrom <FelixWohlfrom@users.noreply.github.com>
2022-10-28 00:22:36 +0200

    Fix - You shouldn't have to many branches in parallel (#63)

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -3,7 +3,6 @@ process.env.DB_USER = 'testUser';
 process.env.DB_PASS = 'testPassword';
 process.env.DB_STORAGE = ':memory:';
 process.env.JWT_SECRET = Math.random(1000);
-process.env.NODE_ENV = 'development';
 
 // Uncomment to debug tests
 //process.env.NODE_ENV = 'development';
