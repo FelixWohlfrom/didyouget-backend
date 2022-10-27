9b2b3e55e9b414b7b2322f0578bdabd196256925
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2022-10-28 00:06:41 +0200

    Add a first testcase to test login behavior

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -2,4 +2,5 @@ process.env.DB_NAME = 'testDb';
 process.env.DB_USER = 'testUser';
 process.env.DB_PASS = 'testPassword';
 process.env.DB_STORAGE = ':memory:';
+process.env.JWT_SECRET = Math.random(1000);
 process.env.NODE_ENV = 'development';
