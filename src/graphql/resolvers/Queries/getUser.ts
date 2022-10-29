4f7ce24b42322cb96bda468bef20feb51da40674
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2022-10-29 20:25:06 +0200

    Fix build

--- src/graphql/resolvers/Queries/getUser.ts
+++ src/graphql/resolvers/Queries/getUser.ts
@@ -1,5 +1,5 @@
 import { Identifier } from "sequelize";
-import { User } from "../../../db/model/index";
+import { User } from "../../../db/model/User";
 
 export const getUser = async (_parent: object, args: { id: Identifier }) => {
     const result = await User.findByPk(args.id);
