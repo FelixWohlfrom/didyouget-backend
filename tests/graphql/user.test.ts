b60add37715e297553d3dc9398f888beef52c4bc
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2022-11-01 01:08:57 +0100

    Add some tests for device api
    To make this easier to handle in the future, move some commonly used
    functionality into a separate file

--- tests/graphql/user.test.ts
+++ tests/graphql/user.test.ts
@@ -1,75 +1,26 @@
-import { Server, IncomingMessage, ServerResponse } from "http";
-import { AddressInfo } from "net";
-import request from "supertest";
-
-import { startApolloServer } from "../src/server";
-
-// The user data for our testuser. Will be used in most of the testcases
-const userData = {
-    username: "TestUser",
-    password: "1234"
-};
-
-// The test server instance
-let server: Server<typeof IncomingMessage, typeof ServerResponse>;
-
-// The authentication token
-let authToken: string | undefined;
-
-/**
- * Will login using the predefined user authentication.
- *
- * @param {boolean} force Force login, even if we are already logged in
- */
-async function login(force = false) {
-    if (!authToken || force) {
-        const response = await runGraphQlQuery({
-            query: `mutation login($userData: userInput!) {
-                login(input: $userData) {
-                    token
-                }
-            }`,
-            variables: { userData: userData }
-        });
-
-        expect(response.body.errors).toBeUndefined();
-        expect(response.body.data?.login.token).not.toBe("");
-
-        authToken = response.body.data.login.token;
-    }
-}
-
-/**
- * Executes a query on the test server.
- * Returns the result.
- *
- * @param {object} queryData The data to pass in the graphql request
- * @return {request.Test} A promise containing the result
- */
-function runGraphQlQuery(queryData: object): request.Test {
-    // send our request to the url of the test server
-    const req = request(
-        `http://localhost:${(server.address() as AddressInfo).port}`
-    ).post("/graphql");
-    if (authToken !== undefined) {
-        req.set("Authorization", `Bearer ${authToken}`);
-    }
-    return req.send(queryData);
-}
+import {
+    userData,
+    authToken,
+    setAuthToken,
+    initServer,
+    stopServer,
+    login,
+    runGraphQlQuery
+} from "./common";
 
 // before the tests we spin up a new Apollo Server
 beforeAll(async () => {
-    server = await startApolloServer(0);
+    await initServer();
 });
 
 // after the tests we'll stop the server
 afterAll(() => {
-    server?.close();
+    stopServer();
 });
 
 describe("an unauthorized user", () => {
     beforeEach(() => {
-        authToken = undefined;
+        setAuthToken(undefined);
     });
 
     it("should be able to register a new user", async () => {
@@ -108,11 +59,11 @@ describe("an unauthorized user", () => {
     it("should not be able to query a single user", async () => {
         const response = await runGraphQlQuery({
             query: `query User {
-            user(id: 1) {
-                id
-                username
-            }
-        }`
+                user(id: 1) {
+                    id
+                    username
+                }
+            }`
         });
         expect(response.body.errors[0].message).toBe("Not Authorised!");
     });
@@ -120,11 +71,11 @@ describe("an unauthorized user", () => {
     it("should not be able to query all users", async () => {
         const response = await runGraphQlQuery({
             query: `query Users {
-            users {
-                id
-                username
-            }
-        }`
+                users {
+                    id
+                    username
+                }
+            }`
         });
         expect(response.body.errors[0].message).toBe("Not Authorised!");
     });
@@ -144,7 +95,7 @@ describe("an authorized user", () => {
             }`
         });
         expect(response.body.data?.logout.success).toBe(true);
-        authToken = undefined;
+        setAuthToken(undefined);
     });
 
     it("should be able to query all users in the system", async () => {
