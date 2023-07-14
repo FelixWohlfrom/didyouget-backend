a93506391a9824fa80c3a92c1e37d76d25fd08d3
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2023-07-14 21:37:46 +0200

    Reformat the file, no code change

--- src/guards/rules/isAuthorized.ts
+++ src/guards/rules/isAuthorized.ts
@@ -2,19 +2,17 @@ import { rule } from "graphql-shield";
 import { Device } from "../../db/model/Device";
 import { DidYouGetLoginData } from "../../utils/auth/model";
 
-export const isAuthorized = rule()(
-    async (
-        _parent: object,
-        _args: object,
-        context: { auth: DidYouGetLoginData | undefined }
-    ) => {
-        if (!(context.auth && context.auth.deviceToken)) {
-            return false;
-        }
-
-        const device = await Device.findOne({
-            where: { token: context.auth.deviceToken, loggedin: true }
-        });
-        return !!device;
+export const isAuthorized = rule()(async (
+    _parent: object,
+    _args: object,
+    context: { auth: DidYouGetLoginData | undefined }
+) => {
+    if (!(context.auth && context.auth.deviceToken)) {
+        return false;
     }
-);
+
+    const device = await Device.findOne({
+        where: { token: context.auth.deviceToken, loggedin: true }
+    });
+    return !!device;
+});
